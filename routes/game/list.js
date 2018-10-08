639056fc3c9513a0db08b1e2109f112c7bb67410
张海斌 <tony@zhanghaibindeMacBook-Pro.local>
2018-10-08 08:24:58 +0800

    update

--- routes/game/list.js
+++ routes/game/list.js
@@ -7,9 +7,8 @@ module.exports = {
     method: 'GET',
     handler(request, reply) {
         const totle = 'select level,title,time,u.name,u.headimgurl from game g,user u where g.user_id=u.id  order by level desc,time asc limit 50';
-        const week = 'select level,title,time,u.name,u.headimgurl from game g,user u where g.user_id=u.id and YEARWEEK(date_format(g.insert_date,\'%Y-%m-%d\')) = YEARWEEK(now())  order by level desc,time asc limit 50';
+        const week = 'select level,title,time,u.name,u.headimgurl from game g,user u where g.user_id=u.id and YEARWEEK(date_format(g.insert_date,\'%Y-%m-%d\') - INTERVAL 1 DAY) = YEARWEEK(now() - INTERVAL 1 DAY)  order by level desc,time asc limit 50';
         const month = 'select level,title,time,u.name,u.headimgurl from game g,user u where g.user_id=u.id and DATE_FORMAT( g.insert_date, \'%Y%m\' ) = DATE_FORMAT( CURDATE( ) ,\'%Y%m\' )  order by level desc,time asc limit 50';
-
         request.app.db.query(totle, (err, tres) => {
             if(err) {
                 request.log(['error'], err);

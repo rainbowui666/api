ae5470251caabae4de19d00e67ddc2bee35948fe
jyhs <jyhs_123@163.com>
2019-03-08 08:36:46 +0800

    update

--- src/api/controller/ad.js
+++ src/api/controller/ad.js
@@ -15,4 +15,18 @@ module.exports = class extends Base {
     });
     this.json({'ad_num': maxId});
   }
+  async getIndexAction() {
+    const list = this.model('mall_ad').where('id<10').select();
+    const ad = {};
+    for (const item of list) {
+      const adArry = ad[item.ad_position_id] || [];
+      ad[item.ad_position_id] = adArry.push(item);
+    }
+    return this.json(ad);
+  }
+  async getAdByPositionIdAction() {
+    const positionId = this.post('positionId');
+    const list = this.model('mall_ad').where({ad_position_id: positionId}).select();
+    return this.json(list);
+  }
 };
